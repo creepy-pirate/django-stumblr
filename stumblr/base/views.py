5d345f8855684fe99a954af3fc3494caebbaab6e
Deepak Singh <dv72276@gmail.com>
2022-03-22 02:02:54 +0530

    completed templates 105

--- stumblr/base/views.py
+++ stumblr/base/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+rooms = [
+    {'id':1,'name':'Lets Learn Python'},
+    {'id':2,'name':'Lets Learn Java'},
+    {'id':3,'name':'Lets Learn C++'},
+]
+
 
 def home(request):
-    return render(request,'home.html')
+    context = {'rooms':rooms}
+    return render(request,'base/home.html',context)
 
-def room(request):
-    return render(request,'room.html')
+def room(request,pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room':room}
+    return render(request,'base/room.html',context)
+
+
 
