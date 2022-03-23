a921bdcb870086dc2387ed47b514d2a7daf7f44a
Deepak Singh <dv72276@gmail.com>
2022-03-23 22:24:54 +0530

    added Models and DataAdmin 1:39

--- stumblr/base/views.py
+++ stumblr/base/views.py
@@ -1,21 +1,14 @@
 from django.shortcuts import render
-
-rooms = [
-    {'id':1,'name':'Lets Learn Python'},
-    {'id':2,'name':'Lets Learn Java'},
-    {'id':3,'name':'Lets Learn C++'},
-]
+from .models import Room
 
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request,'base/home.html',context)
 
 def room(request,pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Room.objects.get(id=pk)
     context = {'room':room}
     return render(request,'base/room.html',context)
 
