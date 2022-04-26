cb6ad056632cb98240835d8c2275c2a0c46ef56b
Deepak Singh <dv72276@gmail.com>
2022-04-27 02:21:25 +0530

    added register login logout 2:41

--- stumblr/base/urls.py
+++ stumblr/base/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/',views.loginPage,name="login"),
+    path('logout/',views.logoutUser,name="logout"),
     path('',views.home,name="home"),
     path('room/<str:pk>/',views.room,name="room"),
     path('create-room/',views.createRoom,name="create-room"),
--- stumblr/base/views.py
+++ stumblr/base/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Room, Topic
 from django.db.models import Q
 from .forms import RoomForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout,authenticate
+
+
+
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request,'User Does not Exists')
+        user = authenticate(request,username=username,password=password)
+        if user:
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,'Username or Password does not exist')
+    context = {}
+    return render(request,'base/login_register.html',context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+
 
 
 def home(request):
