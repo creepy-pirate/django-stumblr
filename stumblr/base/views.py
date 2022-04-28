2c3f1bb6f09fc364620e62e8d05ea04c51ed338a
Deepak Singh <dv72276@gmail.com>
2022-04-28 21:32:39 +0530

    added CRUD for message in room

--- stumblr/base/views.py
+++ stumblr/base/views.py
@@ -1,8 +1,9 @@
+from email import message
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .models import Room, Topic
+from .models import Message, Room, Topic, Message
 from django.db.models import Q
 from .forms import RoomForm
 from django.contrib.auth.models import User
@@ -68,7 +69,18 @@ def home(request):
 
 def room(request,pk):
     room = Room.objects.get(id=pk)
-    context = {'room':room}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room',pk=room.id)
+
+    context = {'room':room,'room_messages':room_messages,'participants':participants}
     return render(request,'base/room.html',context)
 
 
@@ -106,6 +118,17 @@ def deleteRoom(request,pk):
         room.delete()
         return redirect('home')
     return render(request,'base/delete.html',{'obj':room})
+
+
+@login_required(login_url='/login')
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+    if request.user != message.user:
+        return HttpResponse('You are not allowed to do that!')
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')
+    return render(request,'base/delete.html',{'obj':message})
     
 
 
